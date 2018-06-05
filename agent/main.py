236fcdf14b0096c8f94cdd70da407993379e9ecc
Cesar <tratohecho3@gmail.com>
2018-06-04 20:50:15 -0400

    hashing implemented

--- agent/main.py
+++ agent/main.py
@@ -19,13 +19,32 @@ def save_ticker(db_connection, ticker_data=None):
         return False
     if check_if_exists():
         return False
-    
+    ticker_hash = get_ticker_hash(ticker_data)
+    ticker_data['ticker_hash'] = ticker_hash
     ticker_data['rank'] = int(ticker_data['rank'])
     ticker_data['last_updated'] = int(ticker_data['last_updated'])
     db_connection.tickers.insert_one(ticker_data)
     return True
 
 def check_if_exists(db_connection, ticker_data):
-    if db_connection.tickers.find_one({'ticker_hash': 's'}):
+    ticker_hash = get_ticker_hash(ticker_data)
+    if db_connection.tickers.find_one({'ticker_hash': ticker_hash}):
         return True
     return False
+
+def get_hash(value):
+    from hashlib import sha512
+    return sha512(value.encode('utf-8')).hexdigest()
+
+def first_element(elements):
+    return elements[0]
+
+def get_ticker_hash(ticker_data):
+    from collections import OrderedDict
+    ticker_data = OrderedDict(sorted(ticker_data.items(),key=first_element))
+    ticker_value = ''
+
+    for _,value in ticker_data.items():
+        ticker_value += str(value)
+
+    return get_hash(ticker_value)
