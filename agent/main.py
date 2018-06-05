82e1192192027bcab6f1b1eee8190dd2cd34c55f
Cesar <tratohecho3@gmail.com>
2018-06-04 20:03:21 -0400

    save ticker implemented

--- agent/main.py
+++ agent/main.py
@@ -13,3 +13,19 @@ def get_cryptocurrencies_from_api():
         result = r.json()
         return result
     raise Exception('API Error')
+
+def save_ticker(db_connection, ticker_data=None):
+    if not ticker_data:
+        return False
+    if check_if_exists():
+        return False
+    
+    ticker_data['rank'] = int(ticker_data['rank'])
+    ticker_data['last_updated'] = int(ticker_data['last_updated'])
+    db_connection.tickers.insert_one(ticker_data)
+    return True
+
+def check_if_exists(db_connection, ticker_data):
+    if db_connection.tickers.find_one({'ticker_hash': 's'}):
+        return True
+    return False
